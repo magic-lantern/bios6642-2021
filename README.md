5a613c3a22b8f78644e6b6c029397cfd13cd424f
Seth Russell <magic-lantern@users.noreply.github.com>
2021-04-28 17:27:28 -0600

    Saving work

--- README.md
+++ README.md
@@ -8,19 +8,31 @@ Guest lecture on:
 * Web scraping with Python
   * https://news.ycombinator.com/item?id=26090243
   *	https://www.scrapingbee.com/blog/web-scraping-101-with-python/
-  *	Beautifulsoup
+  * https://requests-html.kennethreitz.org/
+
+Problems with many web scraping tools - no javascript support. 
+
+Other options:
+* https://github.com/scrapy/scrapy
+* https://github.com/binux/pyspider
+*	Beautifulsoup4 - https://www.crummy.com/software/BeautifulSoup/
+* Requests-HTML - https://requests-html.kennethreitz.org/ and https://github.com/psf/requests-html Note - this cannot run inside jupyter due to asyncio event loops that conflict. Many bugs currently open with solutions that only work for some.
+* https://github.com/pyppeteer/pyppeteer - Python port of puppeteer JavaScript (headless) chrome/chromium browser automation library.
 
 
 get all csv files from page: https://www.denvergov.org/opendata/dataset/city-and-county-of-denver-crime
 
-Convert table to dataframe
-https://en.wikipedia.org/wiki/List_of_Denver_Broncos_seasons
-https://cubuffs.com/sports/mens-basketball/stats/2019-20
+Convert HTML table to dataframe:
 
-Download data via google drive
-https://drive.google.com/drive/folders/1efQVBclGxwnCCYVLbzH96A0QRSWwmTYI
+* See https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_html.html - doesn't support https, few other gotchas
+* Data Source https://cubuffs.com/sports/mens-basketball/stats/2019-20
+* Data Source https://en.wikipedia.org/wiki/Colorado_Buffaloes_men%27s_basketball
+* https://www.espn.com/mens-college-basketball/team/stats/_/id/38/colorado-buffaloes
 
+Download data via google drive:
 
+* API: https://developers.google.com/drive/api/v3/quickstart/python
+* Colorado COVID data: https://drive.google.com/drive/folders/1efQVBclGxwnCCYVLbzH96A0QRSWwmTYI
 
 To Do List:
 * Feedback form for annual review
--- pandas_web_scraping.py
+++ pandas_web_scraping.py
@@ -5,12 +5,20 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.9.1
+#       jupytext_version: 1.11.1
 #   kernelspec:
 #     display_name: Python [conda env:py37] *
 #     language: python
 #     name: conda-env-py37-py
 # ---
+
+# %%
+# some libraries that are used across examples
+from pprint import pprint
+import re
+
+# %% [markdown]
+# # Web scraping with Pandas
 
 # %%
 import pandas as pd
@@ -37,8 +45,157 @@ print(len(dfs))
 # %%
 dfs[18]
 
+# %% [markdown]
+# # Web Scraping
+
 # %%
-for df in dfs:
-    print(df.head())
+import re
+
+# %%
+url = 'https://www.time.gov/'
+
+# %% [markdown]
+# ## Web scraping using Requests-HTML
+
+# %%
+from requests_html import HTMLSession
+
+# %%
+session = HTMLSession()
+r = session.get(url)
+
+# %%
+page_html = r.html.raw_html.decode('utf-8')
+
+# %%
+html_body = re.search(r'<body>.+</body>', page_html, flags=re.DOTALL)[0]
+lines = re.findall(r'.+', html_body)
+for n in range(20):
+    print(lines[n])
+
+# %%
+r.html.find('time')
+
+# %%
+for time in r.html.find('time'):
+    if 'id' in time.attrs.keys():
+        print(f'UTC Time is: {time.text}')
+
+# %% [markdown]
+# ## Web scraping using Selenium and a web browser
+#
+# For documentation on Selenium, see:
+#
+# * https://selenium-python.readthedocs.io/index.html
+# * https://www.selenium.dev/documentation/en/
+#
+# For a introductory tutorial, see https://www.scrapingbee.com/blog/selenium-python/
+#
+# Selenium requires additional webdriver software that allows you to control Chrome, Firefox, etc. via Python.
+#
+# On macOS, you can use `brew` (https://brew.sh/) to install the dependencies:
+#
+# ```bash
+# brew install geckodriver
+# brew install chromedriver
+# ```
+
+# %%
+from selenium import webdriver
+from IPython.display import Image
+from selenium.webdriver.support.select import Select
+
+# %%
+from selenium.webdriver.firefox.options import Options as firefoxoptions
+options = firefoxoptions()
+# comment out this line to see what's happening
+# options.headless = True
+driver = webdriver.Firefox(options=options)
+driver.set_window_size(1024,768)
+driver.get(url)
+driver.save_screenshot('firefox.png')
+
+# %%
+pprint(driver.capabilities)
+
+# %% [markdown]
+# Alternatively use Chrome
+# ```python
+# from selenium.webdriver.chrome.options import Options as chromeoptions
+# options = chromeoptions()
+# options.headless = True
+# driver = webdriver.Chrome(options=options)
+# driver.set_window_size(640,480)
+# driver.get(url)
+# driver.save_screenshot('chrome.png')
+# ```
+
+# %%
+Image(filename='firefox.png') 
+
+# %%
+page_html = driver.page_source
+
+html_body = re.search(r'<body>.+</body>', page_html, flags=re.DOTALL)[0]
+lines = re.findall(r'.+', html_body)
+for n in range(20):
+    print(lines[n])
+
+# %%
+driver.find_elements_by_tag_name('time')
+
+# %%
+for t in driver.find_elements_by_tag_name('time'):
+    if t.get_attribute('id') is not '':
+        print(t.text)
+
+# %% [markdown]
+# ### A slightly more advanced example
+
+# %%
+driver.get('https://www.amazon.com')
+
+# %%
+search_box = driver.find_element_by_id('twotabsearchtextbox')
+search_box.clear()
+search_box.send_keys('512GB sd card\n')
+search_box.submit()
+
+# %%
+dropdown = driver.find_element_by_class_name('a-dropdown-container')
+dropdown.click()
+
+# %%
+driver.find_element_by_css_selector('.a-dropdown-prompt').click()
+# sort by 'Price: Low to High'
+driver.find_element_by_id('s-result-sort-select_1').click() 
+
+# %%
+# need to see if this workss
+li = driver.find_element_by_css_selector('#p_n_feature_two_browse-bin/13203835011 .a-size-base')
+
+# %%
+li.find_elem
+
+# %%
+sortby
+
+# %%
+
+# %%
+
+# %% [markdown]
+# ### Cleaning up
+#
+# When finished, make sure you close the browser. Can either `driver.quit()` or `driver.close()`. `.close()` will close open tabs. If you close the last tab, it is the same as calling `.quit()`
+
+# %%
+driver.quit()
+
+# %%
+try:
+    driver.close()
+except Exception:
+    print('Already closed')
 
 # %%
